a8a641161295f0a20242ae6592b365976cfb7b62
TecnoSasso <157056700+TecnoSasso@users.noreply.github.com>
2025-05-16 23:31:47 +0200

    Add files via upload

--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,10 @@
 use nalgebra::{DMatrix, DVector};
-use rand::Rng;
+use rand::{rng, seq::SliceRandom, Rng};
 
 // MODULES --------------------------------
 
 mod activations {
-    use nalgebra::DVector;
+    use nalgebra::{DVector};
 
     use crate::Activation;
     fn relu(x: &DVector<f64>) -> DVector<f64> {
@@ -16,10 +16,30 @@ mod activations {
         x.map(|v| 1.0/(1.0+E.powf(-v)))
     }
 
+    fn tanh(x: &DVector<f64>) -> DVector<f64> {
+        use std::f64::consts::E;
+        x.map(|v| {
+            let a = E.powf(v);
+            let b = E.powf(-v);
+            (a - b) / (a + b)
+        })
+    }
+
+    fn softmax(x: &DVector<f64>) -> DVector<f64> {
+        use std::f64::consts::E;
+        let m = x.max();
+        let tot = x.map(|a| E.powf(a-m)).sum();
+        x.map(|v| {
+            E.powf(v-m) / tot
+        })
+    }
+
     pub fn activate(act_fun: &Activation, x: &DVector<f64>) -> DVector<f64>{
         match act_fun {
             Activation::ReLu => return relu(x),
             Activation::Sigmoid => return sigmoid(x),
+            Activation::Tanh => tanh(x),
+            Activation::SoftMax => return softmax(x),
         }
     }
 }
@@ -28,22 +48,20 @@ mod cost_functions {
     use crate::DVector;
     use crate::{CostFunc, NN};
 
-    fn mean_squared_error(batch:Vec<(DVector<f64>, DVector<f64>)>, model:&mut NN, record: bool) -> f64{
+    fn mean_squared_error(batch: &Vec<(DVector<f64>, DVector<f64>)>, model:&mut NN) -> f64{
         let mut sum:f64 = 0.0;
-        for (input, expected) in &batch{
-            let ouput: DVector<f64> = model.ff(input, record);
+        for (input, expected) in batch{
+            let ouput: DVector<f64> = model.ff(input, false);
             let mut cost_vec: DVector<f64> = ouput - expected;
-            let model_len: usize = model.layers.len()-1;  // For BackProp
-            model.layers[model_len].influences = cost_vec.clone().map(|x| 2.0*x);  // For BackProp
             cost_vec = cost_vec.map(|x| x.powi(2));
             sum += cost_vec.sum() as f64;
         }
         sum / (batch.len() as f64)
     }
 
-    pub fn eval_cost(func: CostFunc, batch:Vec<(DVector<f64>, DVector<f64>)>, model:&mut NN, record: bool) -> f64{
+    pub fn eval_cost(func: CostFunc, batch: &Vec<(DVector<f64>, DVector<f64>)>, model:&mut NN) -> f64{
         match func {
-            CostFunc::MeanSquaredError => mean_squared_error(batch, model, record)
+            CostFunc::MeanSquaredError => mean_squared_error(batch, model)
         }
     }
 }
@@ -51,7 +69,7 @@ mod cost_functions {
 mod influences {
     use nalgebra::DVector;
 
-    use crate::Activation;
+    use crate::{activations::activate, Activation};
     fn relu_inf(x: &DVector<f64>) -> DVector<f64> {
         x.map(|v| if v > 0.0 { 1.0 } else { 0.0 })
     }
@@ -64,44 +82,165 @@ mod influences {
         })
     }
 
+    fn tanh_inf(x: &DVector<f64>) -> DVector<f64> {
+        use std::f64::consts::E;
+        x.map(|v| {
+            let a = E.powf(v);
+            let b = E.powf(-v);
+            let t = (a - b) / (a + b);
+            1.0 - t.powi(2)
+        })
+    }
+
+    fn softmax_inf(v: &DVector<f64>) -> DVector<f64> {
+        let a: DVector<f64> = activate(&Activation::SoftMax, v);
+        a.map(|x| {
+            a.map(|y| {
+                if x == y{
+                    x*(1.0-x)
+                }
+                else{
+                    x*-y
+                }
+            }).sum()
+        })
+    }
+
     pub fn act_inf(act_fun: &Activation, x: &DVector<f64>) -> DVector<f64>{
         match act_fun {
             Activation::ReLu => return relu_inf(x),
             Activation::Sigmoid => return sigmoid_inf(x),
+            Activation::Tanh => return tanh_inf(x),
+            Activation::SoftMax => return softmax_inf(x),
         }
     }
 }
 
 mod train_functions {
+    use nalgebra::DMatrix;
+
     use crate::influences::act_inf;
     use crate::TrainFunc;
     use crate::NN;
     use crate::DVector;
-    fn back_prop(model: &mut NN, batch: Vec<(DVector<f64>, DVector<f64>)>) {
-        // Setting up activation influences for the last layer
-        let cost = model.cost(batch, true);
-        println!("Current network cost: {}", cost);
+    
+    fn back_prop(model: &mut NN) -> (Vec<DVector<f64>>, Vec<DMatrix<f64>>){
         // First all the activation influences
-        for i in model.layers.len()-1..2{
+        for i in (1..model.layers.len()).rev() {
             let (prev_slice, next_slice) = model.layers.split_at_mut(i);
             let prev_layer = &mut prev_slice[i-1];
             let curr_layer = &mut next_slice[0];
-            
 
-            let z_on_act = &act_inf(&curr_layer.activ_func, &curr_layer.z);
-            let act_on_cost = &curr_layer.influences;
-            for j in 0..curr_layer.size{
-                prev_layer.influences[j] = (curr_layer.weight_matrix.column(j) * act_on_cost * z_on_act).sum();
+            let z_on_act: &DVector<f64> = &act_inf(&curr_layer.activ_func, &curr_layer.z);
+            let act_on_cost: &DVector<f64> = &curr_layer.influences;
+            for j in 0..=prev_layer.size-1 {
+                let z_on_cost: DVector<f64> = z_on_act.component_mul(act_on_cost);
+                prev_layer.influences[j] = curr_layer.weight_matrix.column(j).dot(&z_on_cost);
             }
         }
-        model.layers.iter().for_each(|l| println!("{}", l.influences));
+
+        // Ok, big part is done, now the bias gradient
+        let mut biases_gradient: Vec<DVector<f64>> = [].to_vec();
+        for i in (1..model.layers.len()).rev() {
+            let curr_layer = &model.layers[i];
+            let z_on_act: &DVector<f64> = &act_inf(&curr_layer.activ_func, &curr_layer.z);
+            let act_on_cost: &DVector<f64> = &curr_layer.influences;
+            let bias_grad: DVector<f64> = z_on_act.component_mul(act_on_cost);
+            biases_gradient.insert(0, bias_grad);
+        }
+
+        // Then weight gradient (a bit tricky)
+        let mut weight_gradient: Vec<DMatrix<f64>> = [].to_vec();
+        for i in (1..=model.layers.len()-1).rev() {
+            let (prev_slice, next_slice) = model.layers.split_at_mut(i);
+            let prev_layer = &mut prev_slice[i-1];
+            let curr_layer = &mut next_slice[0];
+
+            let act_on_cost: &DVector<f64> = &curr_layer.influences;
+            let z_on_act: &DVector<f64> = &act_inf(&curr_layer.activ_func, &curr_layer.z);
+
+            let z_on_cost: &DVector<f64> = &act_on_cost.component_mul(z_on_act);  // const in a row
+            let weight_on_z: &DVector<f64> = &prev_layer.a;  // const in a column
+
+            // Creating the matrix that stores the influences of the weights on the curr layer Zs
+            let mut prev_on_current: DMatrix<f64> = DMatrix::from_columns(&vec![weight_on_z.clone(); curr_layer.size]);
+            prev_on_current = prev_on_current.transpose();
+
+            // Creating the matrix that stores the influences of the Zs on the final cost
+            let curr_on_cost: DMatrix<f64> = DMatrix::from_columns(&vec![z_on_cost.clone(); prev_layer.size]);
+
+            weight_gradient.insert(0, prev_on_current.component_mul(&curr_on_cost));
+        }
+
+        (biases_gradient, weight_gradient)
     }
 
-    pub fn train_model(model: &mut NN, batch: Vec<(DVector<f64>, DVector<f64>)>, func: TrainFunc){
-        match func {
-            TrainFunc::BackProp => back_prop(model, batch),
-            TrainFunc::NEAT => (),
+    fn back_prop_train(model: &mut NN, batch: &Vec<(DVector<f64>, DVector<f64>)>, step: f64){
+        let mut avrege_bias_gradient: Vec<DVector<f64>> = [].to_vec();
+        let mut avrege_weight_gradient: Vec<DMatrix<f64>> = [].to_vec();
+        for (input, expected) in batch{
+            let ouput: DVector<f64> = model.ff(input, true);
+            let cost_vec: DVector<f64> = &ouput - expected;
+
+            // Setting up activation influences for the last layer
+            let model_len: usize = model.layers.len()-1;
+            model.layers[model_len].influences = cost_vec.map(|x| 2.0*x);
+            
+            let (biases_gradien, weight_gradient) = back_prop(model);
+            
+            if avrege_bias_gradient.len() == 0{
+                avrege_bias_gradient = biases_gradien;
+            }
+            else{
+                for i in 0..biases_gradien.len(){
+                    avrege_bias_gradient[i] += &biases_gradien[i];
+                }
+            }
+
+            if avrege_weight_gradient.len() == 0{
+                avrege_weight_gradient = weight_gradient;
+            }
+            else{
+                for i in 0..weight_gradient.len(){
+                    avrege_weight_gradient[i] += &weight_gradient[i];
+                }
+            }
         }
+        avrege_bias_gradient = avrege_bias_gradient.iter().map(
+            |x:&DVector<f64>| x.scale(1.0/batch.len() as f64)).collect();
+
+        avrege_weight_gradient = avrege_weight_gradient.iter().map(
+            |x:&DMatrix<f64>| x.scale(1.0/batch.len() as f64)).collect();
+
+        // Update weight and biases
+        for i in 1..model.layers.len(){
+            model.layers[i].biases -= &avrege_bias_gradient[i-1].scale(step);
+            model.layers[i].weight_matrix -= &avrege_weight_gradient[i-1].scale(step);
+        }
+    }
+
+    pub fn train_model(model: &mut NN, batch: &Vec<(DVector<f64>, DVector<f64>)>, func: &TrainFunc, step: f64){
+        match func {
+            TrainFunc::BackProp => back_prop_train(model, batch, step),
+            TrainFunc::NEAT => (/* TODO */),
+        }
+    }
+}
+
+mod weight_init {
+    use nalgebra::DMatrix;
+    use rand::{rng, Rng};
+    use rand_distr::Normal;
+
+    pub fn glorot(prev_size: usize, next_size: usize, nrows: usize, ncols: usize) -> DMatrix<f64>{
+        let mut rng = rng();
+        let scale = (6.0 / (prev_size + next_size) as f64).sqrt();
+        DMatrix::<f64>::from_fn(nrows, ncols, |_, _| rng.random_range(-scale..=scale))
+    }
+    pub fn he(prev_size: usize, nrows: usize, ncols: usize) -> DMatrix<f64>{
+        let mut rng = rng();
+        let std_dev = (2.0 / prev_size as f64).sqrt();
+        DMatrix::<f64>::from_fn(nrows, ncols, |_, _| rng.sample(Normal::new(0.0, std_dev).unwrap()))
     }
 }
 
@@ -109,6 +248,8 @@ mod train_functions {
 pub enum Activation {
     ReLu,
     Sigmoid,
+    Tanh,
+    SoftMax,
 }
 
 #[derive(Clone)]
@@ -129,22 +270,22 @@ pub struct NN{
 }
 
 impl NN {
-    pub fn ff(&mut self, input: &DVector<f64>, record: bool) -> DVector<f64>{
+    fn ff(&mut self, input: &DVector<f64>, record: bool) -> DVector<f64>{
         let mut act_vec: DVector<f64> = input.clone();
+        if record { self.layers[0].a = input.clone(); }
         for i in 1..self.layers.len() {
             let cl = &mut self.layers[i];
             act_vec = &cl.weight_matrix * act_vec;
             act_vec += &cl.biases;
-            if record {
-                cl.z = act_vec.clone();
-            }
+            if record { cl.z = act_vec.clone(); }  // for backprop
             act_vec = activations::activate(&cl.activ_func, &act_vec);
+            if record { cl.a = act_vec.clone(); }  // for backprop
         }
         act_vec
     }
 
-    pub fn cost(&mut self, batch:Vec<(DVector<f64>, DVector<f64>)>, record: bool) -> f64{
-        cost_functions::eval_cost(self.cost.clone(), batch, self, record)
+    pub fn cost(&mut self, batch: &Vec<(DVector<f64>, DVector<f64>)>) -> f64{
+        cost_functions::eval_cost(self.cost.clone(), batch, self)
     }
 
     pub fn compile(&mut self){
@@ -153,8 +294,16 @@ impl NN {
             panic!("Not enough layers");
         }
         for i in 1..self.layers.len() {
+
             let prev_size = self.layers[i-1].size;
-            self.layers[i].init_matrix(prev_size);
+            
+            if i == self.layers.len()-1{
+                self.layers[i].init_matrix(prev_size, 0);
+            }
+            else{
+                let next_size = self.layers[i+1].size;
+                self.layers[i].init_matrix(prev_size, next_size);
+            }
         }
         // Adding biases
         for i in 1..self.layers.len() {
@@ -162,16 +311,35 @@ impl NN {
         }
     }
     
-    pub fn train(&mut self, batch: Vec<(DVector<f64>, DVector<f64>)>, func: TrainFunc){
-        train_functions::train_model(self, batch, func);
+    pub fn train(&mut self, train_data: &Vec<(DVector<f64>, DVector<f64>)>, batch_size: usize, func: TrainFunc, step: f64){
+        let mut train_data_clone = train_data.clone();
+        
+        let mut rng = rng();
+        train_data_clone.shuffle(&mut rng);
+
+        while train_data_clone.len() > 0 {
+            let mut batch: Vec<(DVector<f64>, DVector<f64>)> = [].to_vec();
+            for _ in 0..batch_size{
+                if let Some(example ) = train_data_clone.pop(){
+                    batch.push(example);
+                }
+                else{ break; }
+            }
+            train_functions::train_model(self, &batch, &func, step);
+        }
     }
 
     pub fn print(&self){
-        println!("-------------------------------------");
+        println!("--------------- WEIGHTS ---------------");
         for i in 1..self.layers.len() {
             println!("{}", self.layers[i].weight_matrix)
         }
-        println!("-------------------------------------");
+        println!("---------------- BIASES ---------------");
+        for i in 1..self.layers.len() {
+            println!("{}", self.layers[i].biases)
+        }
+        println!("---------------------------------------");
+
     }
 }
 
@@ -182,6 +350,7 @@ pub struct Layer{
     biases: DVector<f64>,
     influences: DVector<f64>,
     z: DVector<f64>,
+    a: DVector<f64>,
 }
 
 
@@ -194,18 +363,25 @@ impl Layer {
             biases: DVector::zeros(size),
             influences: DVector::zeros(size),
             z: DVector::zeros(size),
+            a: DVector::zeros(size),
         }
     }
 
-    fn init_matrix(&mut self, prev_size: usize) {
-        let mut rng = rand::rng();
-        let rows = self.size;
-        let cols = prev_size;
-        self.weight_matrix = DMatrix::<f64>::from_fn(rows, cols, |_, _| rng.random_range(0.0..=1.0));
+    fn init_matrix(&mut self, prev_size: usize, next_size: usize) {
+        use rand::rng;
+        let mut rng = rng();
+        match self.activ_func{
+            Activation::Sigmoid => self.weight_matrix = weight_init::glorot(
+                prev_size, next_size, self.size, prev_size
+            ),
+            Activation::ReLu => self.weight_matrix = weight_init::he(prev_size, self.size, prev_size),
+            _ => self.weight_matrix = DMatrix::<f64>::from_fn(
+                self.size, prev_size, |_, _| rng.random_range(0.0..=1.0),
+            )
+        }
     }
 
     fn init_biases(&mut self){
-        let mut rng = rand::rng();
-        self.biases = DVector::<f64>::from_fn(self.size, |_, _| rng.random_range(0.0..=1.0));
+        self.biases = DVector::<f64>::zeros(self.size);
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -5,26 +5,32 @@ use networks::CostFunc::*;
 use networks::TrainFunc::*;
 use nalgebra::DVector;
 
+
+
 fn main() {
     let mut model = NN {
         layers: vec![
-            Layer::linear(2, ReLu),
-            Layer::linear(2, Sigmoid),
-            Layer::linear(2, Sigmoid),
-            Layer::linear(2, Sigmoid),
-            Layer::linear(1, ReLu)
+            Layer::linear(2, Tanh),
+            Layer::linear(4, Tanh),
+            Layer::linear(4, Tanh),
+            Layer::linear(2, SoftMax),
         ],
         cost: MeanSquaredError
     };
     
     model.compile();
 
-    let batch: Vec<(DVector<f64>, DVector<f64>)> = [
-        (DVector::from_vec(vec![0.0, 0.0]), DVector::from_vec(vec![0.0])),
-        (DVector::from_vec(vec![1.0, 0.0]), DVector::from_vec(vec![1.0])),
-        (DVector::from_vec(vec![0.0, 1.0]), DVector::from_vec(vec![1.0])),
-        (DVector::from_vec(vec![1.0, 1.0]), DVector::from_vec(vec![0.0])),
+    let train_data: Vec<(DVector<f64>, DVector<f64>)> = [
+        (DVector::from_vec(vec![0.0, 0.0]), DVector::from_vec(vec![0.0, 1.0])),
+        (DVector::from_vec(vec![1.0, 0.0]), DVector::from_vec(vec![1.0, 0.0])),
+        (DVector::from_vec(vec![0.0, 1.0]), DVector::from_vec(vec![1.0, 0.0])),
+        (DVector::from_vec(vec![1.0, 1.0]), DVector::from_vec(vec![0.0, 1.0])),
     ].to_vec();
 
-    model.train(batch, BackProp);
+    let mut cost: f64;
+    for _ in 0..1000{
+        cost = model.cost(&train_data);
+        println!("{}", cost);
+        model.train(&train_data, 4, BackProp, 1.0);
+    }
 }
